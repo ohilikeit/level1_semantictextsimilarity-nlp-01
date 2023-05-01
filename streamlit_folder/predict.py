fb9661be45b5821295735aec90861f87af81ee58
JiHwan Yoon <yjh091500@naver.com>
2023-05-01 11:29:28 +0900

    add type hint and doc string to predict.py

--- streamlit_folder/predict.py
+++ streamlit_folder/predict.py
@@ -15,7 +15,11 @@ def download_model_file(url):
     gdown.download(url, output, quiet=False)
 
 @st.cache_resource
-def load_model():
+def load_model() -> model.Model:
+    '''
+    Return:
+        model: 구글 드라이브에서 가져온 모델 return 
+    '''
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if not os.path.exists("model.pt"):
         download_model_file(config['model_path'])
@@ -25,7 +29,16 @@ def load_model():
 
     return model
 
-def get_prediction(model, sentence1, sentence2):
+def get_prediction(model: model.Model, sentence1: str, sentence2: str) -> float:
+    '''
+    Args:
+        model: 학습된 모델
+        sentence1: 유사도 측정 문장 1
+        sentence2: 유사도 측정 문장 2
+
+    Return:
+        유사도 측정값
+    '''
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     tokenizer = AutoTokenizer.from_pretrained(config['model_name'])
 
